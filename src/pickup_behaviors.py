6eaecd0782a7d0bb08438e779579c2da226be9ca
Narcis Palomeras <narcispr@gmail.com>
2022-12-07 17:27:22 +0100

    Add behavior classes for environment services: check, get, and let object

---
+++ src/pickup_behaviors.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+
+import py_trees
+import rospy
+from std_srvs.srv import Trigger, TriggerRequest
+
+# Behavior for calling `check_object` task and if True, store object name to Blackboard
+class CheckObject(py_trees.behaviour.Behaviour):
+    def __init__(self, name):
+        super(CheckObject, self).__init__(name)
+        self.blackboard = self.attach_blackboard_client(name=self.name)
+        self.blackboard.register_key(
+            "object_name", access=py_trees.common.Access.WRITE)
+
+    def setup(self):
+        self.logger.debug("  %s [CheckObject::setup()]" % self.name)
+        rospy.wait_for_service('/manage_objects/check_object')
+        try:
+            self.server = rospy.ServiceProxy(
+                '/manage_objects/check_object', Trigger)
+            self.logger.debug(
+                "  %s [CheckObject::setup() Server connected!]" % self.name)
+        except rospy.ServiceException as e:
+            self.logger.debug("  %s [CheckObject::setup() ERROR!]" % self.name)
+
+    def initialise(self):
+        self.logger.debug("  %s [CheckObject::initialise()]" % self.name)
+
+    def update(self):
+        try:
+            self.logger.debug(
+                "  {}: call service /manage_objects/check_object".format(self.name))
+            resp = self.server(TriggerRequest())
+            if resp.success:
+                self.blackboard.object_name = resp.message
+                print("set to blackboard: ", resp.message)
+                return py_trees.common.Status.SUCCESS
+            else:
+                return py_trees.common.Status.FAILURE
+        except:
+            self.logger.debug(
+                "  {}: Error calling service /manage_objects/check_object".format(self.name))
+            return py_trees.common.Status.FAILURE
+
+    def terminate(self, new_status):
+        self.logger.debug("  %s [CheckObject::terminate().terminate()][%s->%s]" %
+                          (self.name, self.status, new_status))
+
+# Behavior for calling `get_object`
+class GetObject(py_trees.behaviour.Behaviour):
+    def __init__(self, name):
+        super(GetObject, self).__init__(name)
+
+    def setup(self):
+        self.logger.debug("  %s [GetObject::setup()]" % self.name)
+        rospy.wait_for_service('/manage_objects/get_object')
+        try:
+            self.server = rospy.ServiceProxy(
+                '/manage_objects/get_object', Trigger)
+            self.logger.debug(
+                "  %s [GetObject::setup() Server connected!]" % self.name)
+        except rospy.ServiceException as e:
+            self.logger.debug("  %s [GetObject::setup() ERROR!]" % self.name)
+
+    def initialise(self):
+        self.logger.debug("  %s [GetObject::initialise()]" % self.name)
+
+    def update(self):
+        try:
+            self.logger.debug(
+                "  {}: call service /manage_objects/get_object".format(self.name))
+            resp = self.server(TriggerRequest())
+            if resp.success:
+                return py_trees.common.Status.SUCCESS
+            else:
+                return py_trees.common.Status.FAILURE
+        except:
+            self.logger.debug(
+                "  {}: Error calling service /manage_objects/get_object".format(self.name))
+            return py_trees.common.Status.FAILURE
+
+    def terminate(self, new_status):
+        self.logger.debug("  %s [GetObject::terminate().terminate()][%s->%s]" %
+                          (self.name, self.status, new_status))
+
+
+# Behavior for calling `let_object`
+class LetObject(py_trees.behaviour.Behaviour):
+    def __init__(self, name):
+        super(LetObject, self).__init__(name)
+
+    def setup(self):
+        self.logger.debug("  %s [LetObject::setup()]" % self.name)
+        rospy.wait_for_service('/manage_objects/let_object')
+        try:
+            self.server = rospy.ServiceProxy(
+                '/manage_objects/let_object', Trigger)
+            self.logger.debug(
+                "  %s [LetObject::setup() Server connected!]" % self.name)
+        except rospy.ServiceException as e:
+            self.logger.debug("  %s [LetObject::setup() ERROR!]" % self.name)
+
+    def initialise(self):
+        self.logger.debug("  %s [LetObject::initialise()]" % self.name)
+
+    def update(self):
+        try:
+            self.logger.debug(
+                "  {}: call service /manage_objects/let_object".format(self.name))
+            resp = self.server(TriggerRequest())
+            if resp.success:
+                return py_trees.common.Status.SUCCESS
+            else:
+                return py_trees.common.Status.FAILURE
+        except:
+            self.logger.debug(
+                "  {}: Error calling service /manage_objects/let_object".format(self.name))
+            return py_trees.common.Status.FAILURE
+
+    def terminate(self, new_status):
+        self.logger.debug("  %s [LetObject::terminate().terminate()][%s->%s]" %
+                          (self.name, self.status, new_status))
+
+# TODO: Create any other required behavior like those to move the robot to a point, 
+#       add or check elements in the blackboard, ...
